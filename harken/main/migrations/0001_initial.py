c9e1927abb27d7ea58cd2a68df6d37602901eedf
Anders Pearson <anders@columbia.edu>
2012-11-16 16:24:00 -0500

    south migration to kick it off

---
+++ harken/main/migrations/0001_initial.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Response'
+        db.create_table('main_response', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('url', self.gf('django.db.models.fields.URLField')(max_length=200)),
+            ('status', self.gf('django.db.models.fields.IntegerField')(default=200)),
+            ('content_type', self.gf('django.db.models.fields.CharField')(default='', max_length=256, blank=True)),
+            ('body', self.gf('django.db.models.fields.TextField')(default='', null=True, blank=True)),
+            ('visited', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal('main', ['Response'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Response'
+        db.delete_table('main_response')
+
+
+    models = {
+        'main.response': {
+            'Meta': {'ordering': "['-visited']", 'object_name': 'Response'},
+            'body': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
+            'content_type': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '256', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'status': ('django.db.models.fields.IntegerField', [], {'default': '200'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'visited': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['main']
